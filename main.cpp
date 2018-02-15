2edce257cf10257208317f15bd2b7f0c8ef18c16
mkalmheiri <mkalmheiri@mail.csuchico.edu>
2018-02-15 12:52:52 -0800

    LuckyNumber

--- main.cpp
+++ main.cpp
@@ -17,17 +17,17 @@ int lucky;
   
   //get user input
 
-cout<< " Write your fist name ? " << endl;
+cout<< " What is your first name? " << endl;
 cin>> a;
 
 
-cout<< " Write your last name ? " << endl;
+cout<< " What is  your last name? " << endl;
 cin>> b;
  
   //tell fortune
 
-cout<<" Welcome " << a[0] << " . " << b[0] << ". " << " , here is your fortune..." <<endl;
-cout<< " Your lucky number is " << a.length() <<endl;
+cout<<" Welcome, " << a[0] << " . " << b[0] << ". " << " , here is your fortune..." <<endl;
+cout<< " your lucky number is " << a.length() <<endl;
  
  if ( a[0]== 'a' || a[0]=='e' || a[0]== 'i' || a[0]== 'o' || a[0]== 'u' || a[0]== 'A' || a[0]== 'E' || a[0]== 'I' || a[0]== 'O' || a[0]== 'U')
 {
@@ -37,7 +37,7 @@ cout << " You are destind to be famous !\n";
 else if ( a[0]>65 && a[0]<90 || a[0]> 97 && a[0]<122 )
 {
 
-cout<< " You should keep a low profile.\n";
+cout<< " you should keep a low profile.\n";
 }
 
 int len= b.length();
@@ -45,13 +45,13 @@ int len= b.length();
 if ( b[len-1] == 'a' || b[len-1] == 'e' || b[len-1] == 'i' || b[len-1] == 'o' || b[len-1] == 'u')
 {
 
-cout<< " You have already met your true love.\n";
-cout<< " Have a good day !\n";
+cout<< " you have already met your true love.\n";
+cout<< " have a good day!\n";
 }
 else
 {
 
-cout<< " Have a good day !\n";
+cout<< " have a good day!\n";
 }
 
   return 0;
