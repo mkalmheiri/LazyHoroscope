85bdbd85a4b03833a2b68ff6c915bf853e53ada9
mkalmheiri <mkalmheiri@mail.csuchico.edu>
2018-02-15 13:04:49 -0800

    LuckyNumber

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ cin>> b;
  
   //tell fortune
 
-cout<<"Welcome, " << a[0] << "." << b[0] << "." << " , here is your fortune..." <<endl;
+cout<<"Welcome, " << a[0] << "." << b[0] << "." << ", here is your fortune..." <<endl;
 cout<< "your lucky number is " << a.length() <<endl;
  
  if ( a[0]== 'a' || a[0]=='e' || a[0]== 'i' || a[0]== 'o' || a[0]== 'u' || a[0]== 'A' || a[0]== 'E' || a[0]== 'I' || a[0]== 'O' || a[0]== 'U')
