ae5198c2444ed2539d630c95a7169c7620356f00
mkalmheiri <mkalmheiri@mail.csuchico.edu>
2018-02-15 13:02:21 -0800

    LuckyNumber

--- main.cpp
+++ main.cpp
@@ -17,22 +17,22 @@ int lucky;
   
   //get user input
 
-cout<< "What is your first name? " << endl;
+cout<< "What is your first name?" << endl;
 cin>> a;
 
 
-cout<< "What is  your last name? " << endl;
+cout<< "What is your last name?" << endl;
 cin>> b;
  
   //tell fortune
 
-cout<<"Welcome, " << a[0] << " . " << b[0] << ". " << " , here is your fortune..." <<endl;
+cout<<"Welcome, " << a[0] << "." << b[0] << "." << " , here is your fortune..." <<endl;
 cout<< "your lucky number is " << a.length() <<endl;
  
  if ( a[0]== 'a' || a[0]=='e' || a[0]== 'i' || a[0]== 'o' || a[0]== 'u' || a[0]== 'A' || a[0]== 'E' || a[0]== 'I' || a[0]== 'O' || a[0]== 'U')
 {
 
-cout << "you are destind to be famous !\n";
+cout << "you are destined to be famous!\n";
 }
 else if ( a[0]>65 && a[0]<90 || a[0]> 97 && a[0]<122 )
 {
